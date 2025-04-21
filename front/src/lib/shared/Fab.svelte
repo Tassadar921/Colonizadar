0abe117a5b2ab3b3d414bc1f23b715a3d26efd3b
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-04-21 23:25:48 +0200

    Update Fab.svelte

--- front/src/lib/shared/Fab.svelte
+++ front/src/lib/shared/Fab.svelte
@@ -1,59 +1,54 @@
 <script lang="ts">
-	import Icon from './Icon.svelte';
+    import Icon from './Icon.svelte';
 
-	export let horizontal: Props['horizontal'] = 'right';
-	export let vertical: Props['vertical'] = 'bottom';
-	export let icon: string = '';
-	export let color: string = 'primary';
-	export let ariaLabel: string;
+    export let horizontal: Props['horizontal'] = 'right';
+    export let vertical: Props['vertical'] = 'bottom';
+    export let icon: string = '';
+    export let color: keyof typeof colorClasses = 'primary';
+    export let ariaLabel: string;
 
-	interface Props {
-		horizontal: 'right' | 'left' | 'middle';
-		vertical: 'top' | 'bottom' | 'center';
-	}
+    interface Props {
+        horizontal: 'right' | 'left' | 'middle';
+        vertical: 'top' | 'bottom' | 'center';
+    }
 
-	interface Horizontal {
-		right: string;
-		left: string;
-		middle: string;
-	}
+    const horizontalClasses = {
+        right: 'right-4',
+        left: 'left-4',
+        middle: 'left-1/2 transform -translate-x-1/2',
+    };
 
-	interface Vertical {
-		top: string;
-		bottom: string;
-		center: string;
-	}
+    const verticalClasses = {
+        top: 'top-4',
+        bottom: 'bottom-4',
+        center: 'top-1/2 transform -translate-y-1/2',
+    };
 
-	const validHorizontal: string[] = ['right', 'left', 'middle'];
-	const validVertical: string[] = ['top', 'bottom', 'center'];
+    const colorClasses = {
+        red: 'bg-red-500 hover:bg-red-900',
+        green: 'bg-green-500 hover:bg-green-900',
+        blue: 'bg-blue-500 hover:bg-blue-900',
+        primary: 'bg-indigo-500 hover:bg-indigo-900', // default fallback
+        gray: 'bg-gray-500 hover:bg-gray-900',
+    };
 
-	$: horizontal = validHorizontal.includes(horizontal) ? horizontal : 'right';
-	$: vertical = validVertical.includes(vertical) ? vertical : 'bottom';
+    const validHorizontal = ['right', 'left', 'middle'];
+    const validVertical = ['top', 'bottom', 'center'];
 
-	const horizontalClasses: Horizontal = {
-		right: 'right-4',
-		left: 'left-4',
-		middle: 'left-1/2 transform -translate-x-1/2',
-	};
-
-	const verticalClasses: Vertical = {
-		top: 'top-4',
-		bottom: 'bottom-4',
-		center: 'top-1/2 transform -translate-y-1/2',
-	};
-
-	$: console.log(`bg-${color}-500 hover:bg-${color}-900`);
+    $: horizontal = validHorizontal.includes(horizontal) ? horizontal : 'right';
+    $: vertical = validVertical.includes(vertical) ? vertical : 'bottom';
+    $: buttonClasses = `text-white shadow-lg flex items-center justify-center fixed size-10 rounded-full
+		transition-colors duration-300
+		${verticalClasses[vertical]}
+		${horizontalClasses[horizontal]}
+		${colorClasses[color] ?? colorClasses['primary']}`;
 </script>
 
 <button
-	on:click
-	aria-label={ariaLabel}
-	style="z-index: 5000"
-	class={`text-white shadow-lg flex items-center justify-center fixed size-10 rounded-full
-        transition-colors duration-300
-        ${verticalClasses[vertical]}
-        ${horizontalClasses[horizontal]}
-        bg-${color}-500 hover:bg-${color}-900`}
+    on:click
+    aria-label={ariaLabel}
+    style="z-index: 5000"
+    class={buttonClasses}
 >
-	<Icon name={icon} />
+    <Icon name={icon} />
 </button>
