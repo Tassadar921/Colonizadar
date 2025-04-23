a1eb75332f4a02f88261d235b6e29c0d6c4a9d5c
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-04-23 15:29:20 +0200

    Update vite.config.ts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+// @ts-ignore
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
