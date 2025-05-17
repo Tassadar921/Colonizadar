2161d054af5bffd465fb97b40118711473cdcb56
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-05-17 21:55:42 +0200

    Update main.ts

--- front/src/main.ts
+++ front/src/main.ts
@@ -5,6 +5,6 @@ import { mount } from 'svelte';
 
 await initLanguages();
 
-const app = mount(App, { target: document.getElementById("app")! });
+const app = mount(App, { target: document.getElementById('app')! });
 
 export default app;
