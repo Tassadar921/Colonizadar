3ecea284e8ab56d74872f8aa6286397322fc0a81
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-05-22 01:40:45 +0200

    Update dbStore.ts

--- front/src/stores/dbStore.ts
+++ front/src/stores/dbStore.ts
@@ -1,4 +1,7 @@
 import Dexie, { type Table } from 'dexie';
+import { readable } from 'svelte/store';
+import { liveQuery } from 'dexie';
+import { showToast } from '../services/toastService';
 
 export interface Move {
     id?: number;
@@ -8,15 +11,67 @@ export interface Move {
     ships: number;
 }
 
-export class MyDatabase extends Dexie {
+export interface Attack {
+    id?: number;
+    from: string;
+    to: string;
+    infantry: number;
+    ships: number;
+}
+
+class FrontDatabase extends Dexie {
     moves!: Table<Move, number>;
+    attacks!: Table<Attack, number>;
 
     constructor() {
         super('colonizadar');
         this.version(1).stores({
             moves: '++id, from, to, infantry, ships',
+            attacks: '++id, from, to, infantry, ships',
         });
     }
 }
 
-export const db = new MyDatabase();
+const db = new FrontDatabase();
+
+export const moves = readable<Move[]>([], (set) => {
+    const subscription = liveQuery(() => db.moves.toArray()).subscribe({
+        next: set,
+        error: (error: any): void => showToast(error.message, 'error'),
+    });
+
+    return (): void => subscription.unsubscribe();
+});
+
+export const attacks = readable<Move[]>([], (set) => {
+    const subscription = liveQuery(() => db.attacks.toArray()).subscribe({
+        next: set,
+        error: (error: any): void => showToast(error.message, 'error'),
+    });
+
+    return (): void => subscription.unsubscribe();
+});
+
+export async function addMove(move: Move): Promise<number> {
+    return db.moves.add(move);
+}
+
+export async function removeMove(id: number): Promise<void> {
+    await db.moves.delete(id);
+}
+
+export async function clearMoves(): Promise<void> {
+    await db.moves.clear();
+}
+
+export async function addAttack(attack: Attack): Promise<number> {
+    return db.attacks.add(attack);
+}
+
+export async function removeAttack(id: number): Promise<void> {
+    await db.attacks.delete(id);
+}
+
+export async function clearAttacks(): Promise<void> {
+    await db.attacks.clear();
+}
