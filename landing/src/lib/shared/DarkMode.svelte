2861365a4413a21f50bc8b22cea998dbda6e5d99
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-06-10 18:43:32 +0200

    Imp ui

--- landing/src/lib/shared/DarkMode.svelte
+++ landing/src/lib/shared/DarkMode.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    import SunIcon from '@lucide/svelte/icons/sun';
-    import MoonIcon from '@lucide/svelte/icons/moon';
+    import { Moon, Sun } from '@lucide/svelte';
     import { ModeWatcher } from 'mode-watcher';
 
     import { toggleMode } from 'mode-watcher';
@@ -10,7 +9,7 @@
 <ModeWatcher />
 
 <Button onclick={toggleMode} variant="outline" size="icon">
-    <SunIcon class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    <MoonIcon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     <span class="sr-only">Toggle theme</span>
 </Button>
--- landing/src/lib/shared/Title.svelte
+++ landing/src/lib/shared/Title.svelte
@@ -2,11 +2,10 @@
     import { onMount } from 'svelte';
 
     export let title: string;
-    export let hasBackground: boolean = false;
 
     onMount((): void => {
         document.title = title;
     });
 </script>
 
-<h1 class="relative z-10 text-3xl {hasBackground ? 'text-white' : ''} font-bold mb-2">{title}</h1>
+<h1 class="relative z-10 text-3xl font-bold mb-2">{title}</h1>
