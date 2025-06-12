2974e2e705074aba69db5b4a6de7f40ec1934a7d
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-06-12 03:02:45 +0200

    Format

--- landing/src/lib/pages/NotFound.svelte
+++ landing/src/lib/pages/NotFound.svelte
@@ -16,13 +16,13 @@
         title: $t('not-found.meta.title'),
         description: $t('not-found.meta.description'),
         images: [
-          {
-            url: `${import.meta.env.VITE_FRONT_URI}/assets/logo-1200x1200.webp`,
-            width: 1200,
-            height: 1200,
-            alt: `open-graph.logo.alt`
-          },
-        ]
+            {
+                url: `${import.meta.env.VITE_FRONT_URI}/assets/logo-1200x1200.webp`,
+                width: 1200,
+                height: 1200,
+                alt: `open-graph.logo.alt`,
+            },
+        ],
     }}
 />
 
